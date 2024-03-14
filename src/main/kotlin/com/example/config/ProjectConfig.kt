3685bf09b01a1e9530868348db63adc72352a53c
aromal.sasidharan <aromalsasidharan@gmail.com>
2024-03-14 23:19:32 +0800

    Multiple mean of same type

--- src/main/kotlin/com/example/config/ProjectConfig.kt
+++ src/main/kotlin/com/example/config/ProjectConfig.kt
@@ -3,20 +3,34 @@ package com.example.config
 import com.example.beans.Vehicle
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
 @Configuration
 open class ProjectConfig {
-    open val vehicle: Vehicle
+    @Primary // in case of multiple bean of same type
+    // we can make a bean to return by default using @Primary annotation thus avoiding exception
     @Bean
-    get() {
+    open fun vehicle(): Vehicle {
         val obj = Vehicle()
-        obj.name = "Vehicle By Spring"
+        obj.name = "Primary Vehicle"
         return obj
     }
 
     @Bean
-    open fun hello(): String = "Hello World"
+    open fun vehicle2(): Vehicle { //Multiple Bean of Same Type
+        val obj = Vehicle()
+        obj.name = "Audi"
+        return obj
+    }
 
+    @Bean("vehicle3")
+    open fun getVehicle3(): Vehicle { //Multiple Bean of Same Type
+        val obj = Vehicle()
+        obj.name = "Maruthi 800"
+        return obj
+    }
+    @Bean
+    open fun hello(): String = "Hello World"
     @Bean
     open fun number(): Int = 43
 }
--- src/main/kotlin/com/example/main/Example1.kt
+++ src/main/kotlin/com/example/main/Example1.kt
@@ -6,15 +6,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main(args: Array<String>) {
+fun main() {
     val vehicle = Vehicle()
     vehicle.name = "Honda City"
     println("Vehicle name from non-spring-context is ${vehicle.name}")
-
     //Creation of IoC Application Context Container
     val context =  AnnotationConfigApplicationContext(ProjectConfig::class.java)
-    val vehicle2:Vehicle = context.getBean(Vehicle::class.java)
+    val primaryVehicle:Vehicle = context.getBean(Vehicle::class.java)
+    println("Vehicle name from spring-context is ${primaryVehicle.name}")
+    val vehicle2:Vehicle = context.getBean("vehicle2",Vehicle::class.java)
     println("Vehicle name from spring-context is ${vehicle2.name}")
+
+    val vehicle3 = context.getBean("vehicle3",Vehicle::class.java)
+    println("Vehicle name from spring-context is ${vehicle3.name}")
     val hello = context.getBean(String::class.java)
     println("String value from spring-context is $hello")
     val num = context.getBean(Integer::class.java)
